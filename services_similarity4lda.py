729d9a1ce4c775597bd15e920d144eabfe141d77
George-sq <275171387@qq.com>
2017-12-06 14:49:52 +0800

    update file

--- services_similarity4lda.py
+++ services_similarity4lda.py
@@ -5,3 +5,22 @@
     @Time   : 2017/12/6 11:26
     @Todo   : 
 """
+
+import warnings
+
+warnings.filterwarnings(action='ignore', category=UserWarning, module='gensim')
+import logging
+from gensim import models
+from gensim import similarities
+import services_textProcess as tps
+import jieba
+
+jieba.setLogLevel(log_level=logging.INFO)
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
