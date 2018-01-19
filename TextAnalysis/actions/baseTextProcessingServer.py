71823686112fc60193f3f40b324ca8fc9899bb3a
George-sq <275171387@qq.com>
2018-01-19 13:29:39 +0800

    update file

--- TextAnalysis/actions/baseTextProcessingServer.py
+++ TextAnalysis/actions/baseTextProcessingServer.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-    @File   : baseTextProcessing.py
+    @File   : baseTextProcessingServer.py
     @Author : NLP_QiangShen (275171387@qq.com)
     @Time   : 2018/1/19 10:05
     @Todo   : 
@@ -8,6 +8,7 @@
 
 from mysqlServer import MysqlServer
 import jieba
+from jieba import posseg
 import logging
 
 jieba.setLogLevel(logging.INFO)
@@ -17,11 +18,12 @@ logger = logging.getLogger(__name__)
 
 def main():
     mysql = MysqlServer(host="10.0.0.247", db="db_pamodata", user="pamo", passwd="pamo")
-    sql = "SELECT * FROM corpus_rawtxts ORDER BY txtId"
+    sql = "SELECT * FROM corpus_rawtxts WHERE txtLabel<>'电信诈骗' ORDER BY txtId LIMIT 10"
     retVal = mysql.executeSql(sql=sql)
-    retVal = [(line[3].strip(), line[0]) for line in retVal[1:]]
-    sql = "UPDATE corpus_rawtxts SET txtContent=%s WHERE txtId=%s"
-    retVal = mysql.executeSql(sql=sql, args=retVal)
+    retVal = [(record[0], record[2], posseg.lcut(record[3])) for record in retVal[1:]]
+    # retVal = [(line[3].strip(), line[0]) for line in retVal[1:]]
+    # sql = "UPDATE corpus_rawtxts SET txtContent=%s WHERE txtId=%s"
+    # retVal = mysql.executeSql(sql=sql, args=retVal)
     print(retVal)
 
 
